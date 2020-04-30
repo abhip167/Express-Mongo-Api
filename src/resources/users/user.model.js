1d990d6f8dd126c79f531f76d83832b7f3e8414e
Abhishek <abhishekpatel167@gmail.com>
2020-04-30 18:47:24 +0530

    Created HASH Password

--- src/resources/users/user.model.js
+++ src/resources/users/user.model.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import pick from "lodash.pick";
+import bcrypt from "bcryptjs";
 
 // Create a Schema ( data modelling)
 const schema = {
@@ -26,5 +28,31 @@ const schema = {
   url: String,
 };
 const userSchema = new mongoose.Schema(schema, { timestamps: true });
+
+// Hash Password before saving to Database
+// Here "this" is the User Object
+userSchema.pre("save", async function (next) {
+  if (this.isModified) {
+    // isModified() bcz jo agar user navo generate thaay che tyare to pass hassh thasej but agar modify thaay che i.e Update to change nai thaay etle isModified lagavanu
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } else {
+    next();
+  }
+});
+
+// Choose What to send to the client
+userSchema.methods.toJSON = function () {
+  const userObject = this.toObject();
+  return pick(userObject, [
+    "_id",
+    "email",
+    "username",
+    "photoUrl",
+    "bio",
+    "url",
+  ]);
+};
 
 export const User = mongoose.model("user", userSchema);
